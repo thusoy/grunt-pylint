edabd994244dc5ff29187cc709ec2d24f7d43dbc
Tarjei Husøy <git@thusoy.com>
2019-04-13 10:40:44 -0700

    Fix inaccurate comment

--- test/fixtures/test_package/unusedvariable.py
+++ test/fixtures/test_package/unusedvariable.py
@@ -1,5 +1,5 @@
 """ This file should trigger linting error for unused variable. """
 
 def unused_variable_func():
-    """ Note the camelcase name and unused variable. Bad bad bad."""
+    """ Note the unused variable. Bad bad bad."""
     unused = 1
